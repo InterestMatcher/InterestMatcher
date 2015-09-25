29e7ed0483c20ec9bab94448858b62e0c7e8683d
hjlynch2 <harris.lynch16@gmail.com>
2015-09-24 22:28:12 -0500

    4 Tommy <3

--- modules/profiles/profileController.js
+++ modules/profiles/profileController.js
@@ -1,15 +1,10 @@
-angular.module('IMapp.profile.controllers', []).controller('ProfileController', function($scope){
-    $scope.profiles= [
+var profileApp = angular.module('IMapp.profile.controllers', [])
+
+profileApp.controller('ProfileController', function($scope) {
+    $scope.profiles = [
     {
-      name: 'Sujay Khandekar',
-      events_created: 3,
-      prof_pic: 'pictures/sujay.jpg'
+      'name': 'Sujay Khandekar',
+      'events_created': '3'
     }
-  ]
-  $scope.plusOne = function(index){
-  	$scope.products[index].likes+=1;
-  }
-  $scope.minusOne = function(index){
-  	$scope.products[index].dislikes+=1;
-  }
+   ];
 });
