6862c88aa2ad74cd2d4c9c62debc4cfdcdb12353
tommypacker <tommypacker@gmail.com>
2015-10-03 17:27:27 -0500

    Continue rewrite

--- modules/profiles/profileController.js
+++ modules/profiles/profileController.js
@@ -2,8 +2,8 @@ var profileApp = angular.module('profileApp', [])
 
 profileApp.controller('ProfileController', function($scope) {
     $scope.samplePerson = {
- 		'name' : Sujay,
- 		'location' : ACM,
- 		'rating' : 5
+ 		name : "Sujay",
+ 		location : "ACM",
+ 		rating : 5
     };
 });
