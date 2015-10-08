4787098e081588fde9ac79b49a03d60c8358c2cf
Lucas Message <lucas.message@hotmail.com>
2015-10-08 15:45:46 -0500

    Linked posts to firebase.

--- modules/posts/js/controllers.js
+++ modules/posts/js/controllers.js
@@ -1,9 +1,19 @@
 
-angular.module('IMapp.posts.controllers', []).controller('PostController', ['$scope', 'postService', function ($scope, postService){
-    $scope.getAllPosts = function () {
-        return postService.getAll();
-    };
-    $scope.posts = $scope.getAllPosts();
+angular.module('IMapp.posts.controllers', []).controller('PostController', ['$scope', 'frontPagePosts', function ($scope, frontPagePosts){
+    $scope.posts = frontPagePosts;
+    
+    var ref = new Firebase("https://interestmatcher.firebaseio.com/posts/chill.com");
+    
+    $scope.addPost = function(){
+        $scope.posts.$add({
+            author: ref.getAuth().facebook.displayName,
+            title: "Hi, I am not a title!",
+            date: new Date().toJSON(),
+            content: $scope.content,
+            comments: {}
+        })
+        
+    }
 
 }]).controller('PostDetailsController', ['$stateParams', '$state', '$scope', 'postService', function ($stateParams, $state, $scope, postService) {
     $scope.getPostById = function (id) {
