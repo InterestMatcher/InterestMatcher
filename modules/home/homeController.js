3460037bad55e56b49f028cc6f4d53cc80ffc8af
whrobbins <wrobbins38@gmail.com>
2015-10-24 15:22:16 -0500

    We need to add many more comments to our codebase...

--- modules/home/homeController.js
+++ modules/home/homeController.js
@@ -22,6 +22,7 @@ app.controller('ChatController',['$scope','publicChatMessages',
 
 		$scope.facebookID = fullID.substring(fbString.length,fullID.length);
 		console.log("Just the ID:"  + $scope.facebookID);
+		
 		// Function used to add a new post.
 		$scope.addMessage = function(){
 
@@ -34,6 +35,10 @@ app.controller('ChatController',['$scope','publicChatMessages',
 
 			// empty the text box when submitted
 			document.getElementById("chatBoxContent").value = '';
+			
+			// attempts to scoll to bottom of chat when chats are sumbitted
+			var objDiv = document.getElementById("chatPane");
+			objDiv.scrollTop = objDiv.scrollHeight;
 			
 			console.log('Data sent?');
 			// Reset title and content.
