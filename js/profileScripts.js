1a3256bb86115bdb4bc7dc64d7ec0289806f8162
hjlynch2 <harris.lynch16@gmail.com>
2015-11-06 14:06:10 -0600

    profile names

--- js/profileScripts.js
+++ js/profileScripts.js
@@ -1,12 +1,9 @@
-
 angular.module('profileModule',['xeditable']);
 
 var profileApp = angular.module('profileModule');
 
 profileApp.controller('ProfileController', function($scope) {
-    $scope.samplePerson = {
- 		name : "Sujay",
- 		location : "ACM",
- 		rating : 5
+    $scope.user = {
+      name: mainRef.getAuth().facebook.displayName,
     };
 });
