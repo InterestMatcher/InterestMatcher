e7d0fe88d94442649b29e6d9e726ad2589039aef
hjlynch2 <harris.lynch16@gmail.com>
2015-11-14 15:34:16 -0600

    deleted facebook implementation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('IMapp', ['postModule', 'profileModule', 'homeModule', 'firebase', 'ui.router', 'loginModule', 'UserApp.facebook-picture']);
+angular.module('IMapp', ['postModule', 'profileModule', 'homeModule', 'firebase', 'ui.router', 'loginModule']);
 
 angular.module('IMapp').run(['$state', function($state) {
     $state.go('loginPage');
