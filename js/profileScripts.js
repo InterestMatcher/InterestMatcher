5a7c0293546b576a11613a1b096beda911af918e
hjlynch2 <harris.lynch16@gmail.com>
2015-12-05 15:49:43 -0600

    prof

--- js/profileScripts.js
+++ js/profileScripts.js
@@ -1,11 +1,9 @@
+
 angular.module('profileModule',['xeditable']);
 
 var profileApp = angular.module('profileModule');
 
 profileApp.controller('ProfileController', function($scope) {
-
-	checkIfLoggedIn($state);
-	
     $scope.user = {
       name: mainRef.getAuth().facebook.displayName,
     };
