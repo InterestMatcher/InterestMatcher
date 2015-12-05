f995f208630afb1ae157a0278dc9d92a4abb729e
hjlynch2 <harris.lynch16@gmail.com>
2015-12-05 16:28:28 -0600

    i hope this works

--- js/profileScripts.js
+++ js/profileScripts.js
@@ -1,10 +1,13 @@
-angular.module('profileModule',['xeditable']);
+angular.module('profileModule');
 
 var profileApp = angular.module('profileModule');
 
 profileApp.controller('ProfileController', function($scope) {
-    $scope.user = {
-      name: mainRef.getAuth().facebook.displayName,
+
+		var ref = new Firebase("https://interestmatcher.firebaseio.com/");
+
+		$scope.user = {
+      name: ref.getAuth().facebook.displayName,
 			authID: ref.getAuth().uid,
     };
 });
